d4f2404b526bc008be181670a7c9a3b7b4364685
Tihomir Dmitrovic <tole.car@gmail.com>
2023-03-09 17:21:36 +0100

    Add chat gpt api support

---
+++ src/Convo/Gpt/Pckg/ChatCompletionElement.php
@@ -0,0 +1,69 @@
+<?php declare(strict_types=1);
+
+namespace Convo\Gpt\Pckg;
+
+use Convo\Core\Workflow\IConversationElement;
+use Convo\Core\Workflow\IConvoRequest;
+use Convo\Core\Workflow\IConvoResponse;
+use Convo\Core\Workflow\AbstractWorkflowContainerComponent;
+use Convo\Core\Params\IServiceParamsScope;
+use Convo\Gpt\GptApiFactory;
+
+class ChatCompletionElement extends AbstractWorkflowContainerComponent implements IConversationElement
+{
+    
+    /**
+     * @var GptApiFactory
+     */
+    private $_gptApiFactory;
+
+    /**
+     * @var IConversationElement[]
+     */
+    private $_ok = [];
+    
+    public function __construct( $properties, $gptApiFactory)
+    {
+        parent::__construct( $properties);
+        
+        $this->_gptApiFactory  =	$gptApiFactory;
+        
+        foreach ( $properties['ok'] as $element) {
+            $this->_ok[] = $element;
+            $this->addChild($element);
+        }
+    }
+    
+    public function read( IConvoRequest $request, IConvoResponse $response)
+    {
+        $messages     =   $this->evaluateString( $this->_properties['messages']);
+
+        $api_key    =   $this->evaluateString( $this->_properties['api_key']);
+        $api        =   $this->_gptApiFactory->getApi( $api_key);
+        
+        $http_response   =   $api->chatCompletion( [
+            'model' => $this->evaluateString( $this->_properties['model']),
+            'messages' => $messages,
+            'temperature' => (float)$this->evaluateString( $this->_properties['temperature']),
+            'max_tokens' => (int)$this->evaluateString( $this->_properties['max_tokens']),
+            'top_p' => (float)$this->evaluateString( $this->_properties['top_p']),
+            'frequency_penalty' => (float)$this->evaluateString( $this->_properties['frequency_penalty']),
+            'presence_penalty' => (float)$this->evaluateString( $this->_properties['presence_penalty']),
+        ]);
+        
+        $params        =    $this->getService()->getComponentParams( IServiceParamsScope::SCOPE_TYPE_REQUEST, $this);
+        $params->setServiceParam( $this->evaluateString( $this->_properties['result_var']), $http_response);
+        
+        foreach ( $this->_ok as $elem)   {
+            $elem->read( $request, $response);
+        }
+    }
+    
+    // UTIL
+    public function __toString()
+    {
+        return parent::__toString().'[]';
+    }
+
+
+}
--- src/Convo/Gpt/Pckg/GptPackageDefinition.php
+++ src/Convo/Gpt/Pckg/GptPackageDefinition.php
@@ -180,6 +180,128 @@ class GptPackageDefinition extends AbstractPackageDefinition
 //                     ],
                 ]
             ),
+            new \Convo\Core\Factory\ComponentDefinition(
+                $this->getNamespace(),
+                '\Convo\Gpt\Pckg\ChatCompletionElement',
+                'GPT Chat Completion',
+                'GPT Chat Completion API call',
+                [
+                    'api_key' => [
+                        'editor_type' => 'text',
+                        'editor_properties' => [],
+                        'defaultValue' => '${API_KEY}',
+                        'name' => 'API key',
+                        'description' => '',
+                        'valueType' => 'string'
+                    ],
+                    'result_var' => [
+                        'editor_type' => 'text',
+                        'editor_properties' => [],
+                        'defaultValue' => 'status',
+                        'name' => 'Result Variable Name',
+                        'description' => 'Status variable containing completion response',
+                        'valueType' => 'string'
+                    ],
+                    'model' => [
+                        'editor_type' => 'select',
+                        'editor_properties' => [
+                            'options' => [ 
+                                'gpt-3.5-turbo' => 'gpt-3.5-turbo', 
+                                'gpt-3.5-turbo-0301' => 'gpt-3.5-turbo-0301', 
+                            ],
+                        ],
+                        'defaultValue' => 'gpt-3.5-turbo',
+                        'name' => 'Model',
+                        'description' => '',
+                        'valueType' => 'string'
+                    ],
+                    'messages' => [
+                        'editor_type' => 'text',
+                        'editor_properties' => [],
+                        'defaultValue' => '',
+                        'name' => 'messages',
+                        'description' => '',
+                        'valueType' => 'string'
+                    ],
+                    'temperature' => [
+                        'editor_type' => 'text',
+                        'editor_properties' => [],
+                        'defaultValue' => 0.7,
+                        'name' => 'Temperature',
+                        'description' => '',
+                        'valueType' => 'string'
+                    ],
+                    'max_tokens' => [
+                        'editor_type' => 'text',
+                        'editor_properties' => [],
+                        'defaultValue' => 256,
+                        'name' => 'Max tokens',
+                        'description' => '',
+                        'valueType' => 'string'
+                    ],
+                    'top_p' => [
+                        'editor_type' => 'text',
+                        'editor_properties' => [],
+                        'defaultValue' => 1,
+                        'name' => 'Top p',
+                        'description' => '',
+                        'valueType' => 'string'
+                    ],
+                    'frequency_penalty' => [
+                        'editor_type' => 'text',
+                        'editor_properties' => [],
+                        'defaultValue' => 0,
+                        'name' => 'Frequency penalty',
+                        'description' => '',
+                        'valueType' => 'string'
+                    ],
+                    'presence_penalty' => [
+                        'editor_type' => 'text',
+                        'editor_properties' => [],
+                        'defaultValue' => 0,
+                        'name' => 'Presence penalty',
+                        'description' => '',
+                        'valueType' => 'string'
+                    ],
+                    'ok' => [
+                        'editor_type' => 'service_components',
+                        'editor_properties' => [
+                            'allow_interfaces' => ['\Convo\Core\Workflow\IConversationElement'],
+                            'multiple' => true
+                        ],
+                        'defaultValue' => [],
+                        'defaultOpen' => false,
+                        'name' => 'OK flow',
+                        'description' => 'Flow to be executed if operation is finished with result variable available for use',
+                        'valueType' => 'class'
+                    ],
+                    '_preview_angular' => [
+                        'type' => 'html',
+                        'template' => '<div class="code"><span class="statement">CHAT COMPLETION</span>' .
+                        ' <br>  {{component.properties.prompt}} ' .
+                        '</div>'
+                    ],
+                    '_interface' => '\Convo\Core\Workflow\IConversationElement',
+                    '_workflow' => 'read',
+                    '_factory' => new class ( $this->_gptApiFactory) implements \Convo\Core\Factory\IComponentFactory
+                    {
+                        private $_gptApiFactory;
+                        
+                        public function __construct( $gptApiFactory)
+                        {
+                            $this->_gptApiFactory	   =   $gptApiFactory;
+                        }
+                        public function createComponent( $properties, $service)
+                        {
+                            return new ChatCompletionElement( $properties, $this->_gptApiFactory);
+                        }
+                    }
+//                     '_help' =>  [
+//                         'type' => 'file',
+//                         'filename' => 'voice-response-element.html'
+//                     ],
+                ]
+            ),
         ];
     }
 }
