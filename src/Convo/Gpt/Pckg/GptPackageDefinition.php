ad2ce5dc452f6dadec8548950a8dcc0f7494af03
Tihomir Dmitrovic <tole.car@gmail.com>
2023-08-04 18:58:43 +0200

    Register tokenize_string() function

--- src/Convo/Gpt/Pckg/GptPackageDefinition.php
+++ src/Convo/Gpt/Pckg/GptPackageDefinition.php
@@ -5,6 +5,7 @@ namespace Convo\Gpt\Pckg;
 use Convo\Core\Factory\AbstractPackageDefinition;
 use Convo\Core\Factory\PackageProviderFactory;
 use Convo\Gpt\GptApiFactory;
+use Convo\Core\Expression\ExpressionFunction;
 
 class GptPackageDefinition extends AbstractPackageDefinition 
 {
@@ -29,6 +30,45 @@ class GptPackageDefinition extends AbstractPackageDefinition
         parent::__construct( $logger, self::NAMESPACE, __DIR__);
         
         $this->registerTemplate( __DIR__ .'/gpt-examples.template.json');
+    }
+    
+    public function getFunctions()
+    {
+        $functions = [];
+        
+        $stop_words = [
+            "a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "aren't", "as", "at",
+            "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can't", "cannot", "could",
+            "couldn't", "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during", "each", "few", "for", "from",
+            "further", "had", "hadn't", "has", "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here",
+            "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in",
+            "into", "is", "isn't", "it", "it's", "its", "itself", "let's", "me", "more", "most", "mustn't", "my", "myself", "no",
+            "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own",
+            "same", "shan't", "she", "she'd", "she'll", "she's", "should", "shouldn't", "so", "some", "such", "than", "that", "that's",
+            "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're",
+            "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "wasn't", "we", "we'd", "we'll",
+            "we're", "we've", "were", "weren't", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who",
+            "who's", "whom", "why", "why's", "with", "won't", "would", "wouldn't", "you", "you'd", "you'll", "you're", "you've",
+            "your", "yours", "yourself", "yourselves"
+        ];
+        
+        
+        $functions[] = new ExpressionFunction(
+            'tokenize_string',
+            function ($text) {
+                return sprintf('tokenize_string(%s)', var_export( $text, true));
+            },
+            function($args, $text) use ( $stop_words) {
+                $text = wp_strip_all_tags( $text);
+                $text = strtolower( $text);
+                $text = preg_replace("#[[:punct:]]#", "", $text);
+                $tokens = explode(' ', $text);
+                $meaningful_tokens = array_diff( $tokens, $stop_words);
+                return implode( ' ', $meaningful_tokens);
+            }
+            );
+        
+        return $functions;
     }
     
     protected function _initDefintions()
