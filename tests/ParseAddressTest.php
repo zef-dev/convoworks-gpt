095fb11121cd346bc1f12596e5576e07672e76cd
Tihomir Dmitrovic <tole.car@gmail.com>
2023-10-26 16:05:31 +0200

    Add testfor json processing

--- tests/ParseAddressTest.php
+++ tests/ParseAddressTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
+use Convo\Gpt\Pckg\ChatCompletionV2Element;
 
 class ParseAddressTest extends TestCase
 {
@@ -56,21 +57,56 @@ class ParseAddressTest extends TestCase
      */
     public function testPreprocessJson( $inputJson, $expectedJson)
     {
-        $result = self::preprocessJsonWithConstants($inputJson);
+        $result = ChatCompletionV2Element::preprocessJsonWithConstants($inputJson);
         $this->assertEquals( $expectedJson, $result, "Failed asserting that two strings are equal.");
     }
     
-    
-    public static function preprocessJsonWithConstants( $json)
+    public function processDataProvider()
     {
-        // GREAT ONE
-        return preg_replace_callback('/"([^"]+)"|([A-Z_]+)/', function ($matches) {
-            // If it is a constant (not enclosed by quotes), add quotes
-            if (!empty($matches[2]) && !preg_match('/"[A-Z_]+"/', $matches[2])) {
-                return '"' . $matches[2] . '"';
-            }
-            // If it is within quotes, return as is
-            return $matches[0];
-        }, $json);
+        return [
+            [
+                '{ "a": "Here is some FILE_APPEND text", "b": FILE_APPEND }',
+                '{ "a": "Here is some FILE_APPEND text", "b": '.json_encode( FILE_APPEND).' }'
+            ],
+            [
+                '{"constant":CONSTANT_VALUE}',
+                '{"constant":"CONSTANT_VALUE"}'
+            ],
+            [
+                '["element1", "element2", ELEMENT]',
+                '["element1", "element2", "ELEMENT"]'
+            ],
+            [
+                '{"mixed": "Text ELEMENT"}',
+                '{"mixed": "Text ELEMENT"}'
+            ],
+            
+            [
+                '{"value": CONSTANT, "number": 123}',
+                '{"value": "CONSTANT", "number": 123}'
+            ],
+            [
+                '{"object": {"nested_constant": NESTED_CONSTANT}}',
+                '{"object": {"nested_constant": "NESTED_CONSTANT"}}'
+            ],
+            [
+                '{"boolean_true": TRUE, "boolean_false": FALSE, "null_value": NULL}',
+                '{"boolean_true": true, "boolean_false": false, "null_value": null}'
+            ],
+            [
+                '{CONSTANT_KEY: "value"}',
+                '{"CONSTANT_KEY": "value"}'
+            ]
+        ];
     }
+    
+    /**
+     * @dataProvider processDataProvider
+     */
+    public function testProcessJson( $inputJson, $expectedData)
+    {
+        $result = ChatCompletionV2Element::processJsonWithConstants( $inputJson);
+        $this->assertEquals( $expectedData, $result, "Failed asserting that two objects are equal.");
+    }
+    
 }
