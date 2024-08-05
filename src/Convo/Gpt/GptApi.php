fca4b6d6dd210e6051eb70f35226d9cf394e753d
Tihomir Dmitrovic <tole.car@gmail.com>
2024-08-05 23:21:16 +0200

    Less logging

--- src/Convo/Gpt/GptApi.php
+++ src/Convo/Gpt/GptApi.php
@@ -9,17 +9,17 @@ use Convo\Core\Util\HttpClientException;
 class GptApi
 {
     private $_apiKey;
-    
+
     /**
      * @var IHttpFactory
      */
     private $_httpFactory;
-    
+
     /**
      * @var \Psr\Log\LoggerInterface
      */
     private $_logger;
-    
+
     public function __construct( $logger, $httpFactory, $apiKey)
     {
         $this->_logger = $logger;
@@ -37,22 +37,22 @@ class GptApi
         $headers = [
             'Authorization' => 'Bearer '.$this->_apiKey,
         ];
-        
+
         $this->_logger->debug( 'Http request data ['.print_r( $data, true).']');
-        
+
         $config = [];
-        
+
         $client = $this->_httpFactory->getHttpClient( $config);
         $request = $this->_httpFactory->buildRequest( IHttpFactory::METHOD_POST, 'https://api.openai.com/v1/completions', $headers, $data);
-        
+
         $response = $client->sendRequest( $request);
-        
+
         $response_raw = $response->getBody()->getContents();
-        
+
         $response_data = json_decode( $response_raw, true);
-        
+
         $this->_logger->debug( 'Http response data ['.print_r( $response_data, true).']');
-        
+
         return $response_data;
     }
 
@@ -61,14 +61,14 @@ class GptApi
         $headers = [
             'Authorization' => 'Bearer '.$this->_apiKey,
         ];
-        
+
         $this->_logger->debug( 'Http request data ['.print_r( $data, true).']');
-        
+
         $config = [];
-        
+
         $client = $this->_httpFactory->getHttpClient( $config);
         $request = $this->_httpFactory->buildRequest( IHttpFactory::METHOD_POST, 'https://api.openai.com/v1/chat/completions', $headers, $data);
-        
+
         try {
             $response = $client->sendRequest( $request);
         } catch ( HttpClientException $e) {
@@ -80,81 +80,81 @@ class GptApi
             }
             throw $e;
         }
-        
+
         $response_data = json_decode( $response->getBody()->getContents(), true);
-        
+
         $this->_logger->debug( 'Http response data ['.print_r( $response_data, true).']');
-        
+
         return $response_data;
     }
-    
+
 
     public function embeddings( $data)
     {
         $headers = [
             'Authorization' => 'Bearer '.$this->_apiKey,
         ];
-        
+
         $this->_logger->debug( 'Http request data ['.print_r( $data, true).']');
-        
+
         $config = [];
-        
+
         $client = $this->_httpFactory->getHttpClient( $config);
         $request = $this->_httpFactory->buildRequest( IHttpFactory::METHOD_POST, 'https://api.openai.com/v1/embeddings', $headers, $data);
-        
+
         $response = $client->sendRequest( $request);
-        
+
         $response_data = json_decode( $response->getBody()->getContents(), true);
-        
-        $this->_logger->debug( 'Http response data ['.print_r( $response_data, true).']');
-        
+
+      //  $this->_logger->debug( 'Http response data ['.print_r( $response_data, true).']');
+
         return $response_data;
     }
-    
+
     public function moderations( $data)
     {
         $headers = [
             'Authorization' => 'Bearer '.$this->_apiKey,
         ];
-        
+
         $this->_logger->debug( 'Http request data ['.print_r( $data, true).']');
-        
+
         $config = [];
-        
+
         $client = $this->_httpFactory->getHttpClient( $config);
         $request = $this->_httpFactory->buildRequest( IHttpFactory::METHOD_POST, 'https://api.openai.com/v1/moderations', $headers, $data);
-        
+
         $response = $client->sendRequest( $request);
-        
+
         $response_data = json_decode( $response->getBody()->getContents(), true);
-        
+
         $this->_logger->debug( 'Http response data ['.print_r( $response_data, true).']');
-        
+
         return $response_data;
     }
-    
+
     public function createImage( $data)
     {
         $headers = [
             'Authorization' => 'Bearer '.$this->_apiKey,
         ];
-        
+
         $this->_logger->debug( 'Performing request');
-        
+
         $config = [];
-        
+
         $client = $this->_httpFactory->getHttpClient( $config);
         $request = $this->_httpFactory->buildRequest( IHttpFactory::METHOD_POST, 'https://api.openai.com/v1/images/generations', $headers, $data);
-        
+
         $response = $client->sendRequest( $request);
-        
+
         $response_data = json_decode( $response->getBody()->getContents(), true);
-        
+
         $this->_logger->debug( 'Http response ['.$response->getStatusCode().']');
-        
+
         return $response_data;
     }
-    
+
     // UTIL
     public function __toString()
     {
